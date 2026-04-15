d66af1e87f2857eb2e35737fe5811b46e0a0a98f
Jameson Taylor <jameson@builder.io>
2026-04-15 15:42:38 -0400

    initial commit;

---
+++ src/components/ProviderComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ProviderComponent.css';
+
+const Provider: React.FC = () => {
+  return (
+    <div>
+      <h1>Remote Feature 1</h1>
+    </div>
+  );
+};
+
+export default Provider;
